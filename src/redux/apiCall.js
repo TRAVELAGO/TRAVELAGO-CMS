6d969ec600b3487789ce61c231bc16675efc4d3b
ktshglsm <ktshglsm35@gmail.com>
2024-04-13 19:58:04 +0700

    first api

--- src/redux/apiCall.js
+++ src/redux/apiCall.js
@@ -11,6 +11,8 @@ export const login = async (dispatch, user) => {
     dispatch(loginStart());
     try {
         const res = await makeRequest.post("auth/login", user);
+        const accessToken = res.data.accessToken;
+        makeRequest.defaults.headers.common = { 'Authorization': `bearer ${accessToken}` }
         dispatch(loginSuccess(res.data));
         document.cookie = `session=${JSON.stringify(res.data)}; path=/;`;
     } catch (error) {
