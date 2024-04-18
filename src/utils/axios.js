1c23d3651b8b33bba5f25b33b5e0dcd9faa7d5be
Minh Duc <minhduc.mll@gmail.com>
2024-04-18 18:06:09 +0700

    Feat: add loading component

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+
 export const makeRequest = axios.create({
-    baseURL: "http://localhost:8888/",
+    baseURL: import.meta.env.VITE_BASE_URL,
     withCredentials: true,
 })
 // baseURL: "https://travelago-api.onrender.com/",
