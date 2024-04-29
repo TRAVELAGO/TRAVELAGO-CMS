04f08a92ff3d4388d321c65eced2ac995d1355bd
MinhDuc <minhduc.mll@gmail.com>
2024-04-29 18:37:32 +0700

    Feat: create menu component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,12 +6,33 @@ import { fetchLogout } from "../../redux/userAction";
 import { PATH_URL } from "../../utils/const/common";
 import Avatar from "../base/avatar/Avatar";
 import "./navbar.scss";
+import Menu from "../base/menu/Menu";
+
+const PROFILE_MENU = [
+  {
+    name: "Profile",
+    link: PATH_URL.PROFILE,
+  },
+  {
+    name: "My Booking",
+    link: PATH_URL.PROFILE_BOOKING,
+  },
+  {
+    name: "Wishlist",
+    link: PATH_URL.WISHLIST,
+  },
+];
 
 function Navbar() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user.currentUser);
   const [open, setOpen] = useState(false);
+  const [openProfileMenu, setOpenProfileMenu] = useState(false);
+
+  const handleOpenMenu = () => {
+    setOpenProfileMenu(true);
+  };
 
   const handleLogout = () => {
     fetchLogout(dispatch);
@@ -32,23 +53,52 @@ function Navbar() {
       </div>
       <div className="right">
         {user ? (
-          <div className="user">
-            <Link to={PATH_URL.PROFILE}>
-              <Avatar url={user.avatar} name={user.fullName} noti={3} />
-              <span>{user.fullName}</span>
-            </Link>
-            <Link to={PATH_URL.WISHLIST} className="my-booking">
-              <span>Wishlist</span>
-            </Link>
-            <Link to={PATH_URL.PROFILE_BOOKING} className="my-booking">
-              <span>My Booking</span>
-            </Link>
-            <Link to="" className="logout" onClick={handleLogout}>
-              Log out
-            </Link>
+          <div className="user h-full">
+            <div className="relative flex items-center h-full">
+              {/* <Link to={PATH_URL.PROFILE}> */}
+              <Link onClick={handleOpenMenu}>
+                <Avatar url={user.avatar} name={user.fullName} noti={3} />
+                <span>{user.fullName}</span>
+              </Link>
+              <Menu
+                isOpen={openProfileMenu}
+                onClose={() => setOpenProfileMenu(false)}
+              >
+                <ul>
+                  {PROFILE_MENU.map((menu) => (
+                    <li
+                      key={menu.name}
+                      className="relative block hover:border-l-2 hover:border-primary-100"
+                    >
+                      <Link
+                        to={menu.link}
+                        className="inline-block w-full text-dark-100 border-b border-gray-100 px-5 py-3 hover:text-primary-100 hover:pl-6"
+                      >
+                        <span className="text-base font-bold capitalize">
+                          {menu.name}
+                        </span>
+                      </Link>
+                    </li>
+                  ))}
+                  <li
+                    key={"Logout"}
+                    className="relative block hover:border-l-2 hover:border-primary-100"
+                  >
+                    <Link
+                      className="inline-block w-full text-dark-100 px-5 py-3 hover:text-primary-100 hover:pl-6"
+                      onClick={handleLogout}
+                    >
+                      <span className="text-base font-bold capitalize">
+                        Logout
+                      </span>
+                    </Link>
+                  </li>
+                </ul>
+              </Menu>
+            </div>
           </div>
         ) : (
-          <>
+          <div className="auth">
             <Link to={PATH_URL.HOTEL_REGISTER} className="host-btn">
               Become a Hosts
             </Link>
@@ -58,7 +108,7 @@ function Navbar() {
             <Link to={PATH_URL.REGISTER} className="register-btn">
               Sign up
             </Link>
-          </>
+          </div>
         )}
         <div className="menuIcon">
           <img
@@ -72,8 +122,13 @@ function Navbar() {
           <Link to={PATH_URL.ABOUT}>About</Link>
           <Link to={PATH_URL.CONTACT}>Contact</Link>
           <Link to={PATH_URL.HOTEL}>Agents</Link>
-          <Link to={PATH_URL.LOGIN}>Sign in</Link>
-          <Link to={PATH_URL.REGISTER}>Sign up</Link>
+          {!user && (
+            <>
+              <Link to={PATH_URL.HOTEL_REGISTER}>Become a Hosts</Link>
+              <Link to={PATH_URL.LOGIN}>Sign in</Link>
+              <Link to={PATH_URL.REGISTER}>Sign up</Link>
+            </>
+          )}
         </div>
       </div>
     </nav>
