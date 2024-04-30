48ab12f54c8caf96171e2cccd654b4ce7b0e9f87
MinhDuc <minhduc.mll@gmail.com>
2024-04-30 20:21:33 +0700

    Feat: create room card new ui

--- src/routes/wishlist/Wishlist.jsx
+++ src/routes/wishlist/Wishlist.jsx
@@ -5,6 +5,7 @@ import List from "../../components/list/List";
 import { withLoading } from "../../redux/appAction";
 import { fetchInitWishlist } from "../../redux/wishlistAction";
 import "./wishlist.scss";
+import CardImage from "../../components/cardImage/CardImage";
 
 function Wishlist() {
   const dispatch = useDispatch();
@@ -26,7 +27,10 @@ function Wishlist() {
             <h1>My Wishlist</h1>
           </div>
           <div className="border-t border-[#eee]"></div>
-          <List data={wishlist} />
+          {/* <List data={wishlist} /> */}
+          {wishlist?.map((item) => (
+            <CardImage item={item} />
+          ))}
         </div>
       </div>
       <div className="chatContainer">
