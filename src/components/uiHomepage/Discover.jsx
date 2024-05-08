14f84d984733003fd6f028186c130a64b03746fb
Dung24-6 <hadung24062002@gmail.com>
2024-05-08 14:50:49 +0700

    feature/UI-homepage

---
+++ src/components/uiHomepage/Discover.jsx
@@ -0,0 +1,194 @@
+import "./uiHomepage.scss";
+function Discover() {
+  return (
+    <div data-subtree="PopularRoutesV2">
+      <div className="css-1dbjc4n">
+        <div className="css-1dbjc4n r-cqs64b r-1fnihl3">
+          <div className="css-1dbjc4n r-eqz5dr" style={{ alignItems: "stretch", WebkitBoxAlign: "stretch" }}>
+            <h1
+              aria-level="1"
+              dir="auto"
+              role="heading"
+              className="css-4rbku5 css-901oao r-cwxd7f r-t1w4ow r-1x35g6 r-b88u0q r-vrz42v r-fdjqy7"
+            >
+              Bạn muốn khám phá điều gì?
+            </h1>
+            <div className="css-1dbjc4n" style={{ paddingBottom: 24 }}></div>
+            <div className="css-1dbjc4n r-13qz1uu">
+              <div className="css-1dbjc4n r-1vo5lcr r-wgabs5">
+                <div className="css-1dbjc4n r-18u37iz">
+                  <div className="css-1dbjc4n r-mbgqwd">
+                    <div
+                      tabIndex="0"
+                      className="css-1dbjc4n r-1loqt21 r-1yzf0co r-1otgn73 r-1i6wzkk r-lrvibr"
+                      data-testid="undefined-HOTEL"
+                      style={{ transitionDuration: "0.25s" }}
+                    >
+                      <h3
+                        aria-level="3"
+                        dir="auto"
+                        role="heading"
+                        className="css-4rbku5 css-901oao r-t1w4ow r-ubezar r-b88u0q r-rjixqe r-1udh08x r-q4m81j r-1udbk01 r-3s2u2q"
+                        style={{ color: "rgb(104, 113, 118)" }}
+                      >
+                        Các khách sạn hàng đầu
+                      </h3>
+                    </div>
+                    <div
+                      className="css-1dbjc4n r-1bxrh7q r-waaub4 r-1p0dtai r-1w2pmg r-u8s1d"
+                      style={{ backgroundColor: "rgb(1, 148, 243)", transformOrigin: "left top", width: 181 }}
+                    ></div>
+                  </div>
+                </div>
+                <div
+                  className="css-1dbjc4n r-1bxrh7q r-waaub4 r-1p0dtai r-u8s1d"
+                  style={{
+                    backgroundColor: "rgb(1, 148, 243)",
+                    height: 4,
+                    transform: "translateX(0px)",
+                    transformOrigin: "left top",
+                    width: 182,
+                  }}
+                ></div>
+              </div>
+              <div className="css-1dbjc4n r-6koalj r-13awgt0" style={{ opacity: 1, transform: "translateX(0px)" }}>
+                <div
+                  dir="auto"
+                  className="css-901oao r-cwxd7f r-6koalj r-13awgt0 r-18u37iz r-t1w4ow r-ubezar r-majxgm r-1wtj0ep r-135wba7 r-11c0sde r-fdjqy7 r-13qz1uu"
+                ></div>
+              </div>
+              <div class="css-1dbjc4n r-13awgt0" style={{ opacity: 1, transform: "translateX(0px)" }}>
+                <div
+                  dir="auto"
+                  class="css-901oao r-cwxd7f r-6koalj r-13awgt0 r-18u37iz r-t1w4ow r-ubezar r-majxgm r-1wtj0ep r-135wba7 r-11c0sde r-fdjqy7 r-13qz1uu"
+                >
+                  <span class="css-901oao css-16my406 r-cwxd7f r-6koalj r-eqz5dr r-t1w4ow r-ubezar r-majxgm r-135wba7 r-fdjqy7 r-30z84g">
+                    <a href="https://travelago.vercel.app/" class="css-4rbku5" style={{ paddingBottom: "24px" }}>
+                      <span
+                        class="css-901oao css-16my406 css-cens5h r-t1w4ow r-1b43r93 r-majxgm r-rjixqe r-fdjqy7"
+                        style={{ WebkitLineClamp: 2, color: "rgb(104, 113, 118)" }}
+                      >
+                        Khách sạn Vũng Tàu
+                      </span>
+                    </a>
+                    <a href="https://travelago.vercel.app/" class="css-4rbku5" style={{ paddingBottom: "24px" }}>
+                      <span
+                        class="css-901oao css-16my406 css-cens5h r-t1w4ow r-1b43r93 r-majxgm r-rjixqe r-fdjqy7"
+                        style={{ WebkitLineClamp: 2, color: "rgb(104, 113, 118)" }}
+                      >
+                        Khách sạn Đà Lạt
+                      </span>
+                    </a>
+                    <a href="https://travelago.vercel.app/" class="css-4rbku5" style={{ paddingBottom: "24px" }}>
+                      <span
+                        class="css-901oao css-16my406 css-cens5h r-t1w4ow r-1b43r93 r-majxgm r-rjixqe r-fdjqy7"
+                        style={{ WebkitLineClamp: 2, color: "rgb(104, 113, 118)" }}
+                      >
+                        Khách sạn Đà Nẵng
+                      </span>
+                    </a>
+                    <a href="https://travelago.vercel.app/" class="css-4rbku5" style={{ paddingBottom: "24px" }}>
+                      <span
+                        class="css-901oao css-16my406 css-cens5h r-t1w4ow r-1b43r93 r-majxgm r-rjixqe r-fdjqy7"
+                        style={{ WebkitLineClamp: 2, color: "rgb(104, 113, 118)" }}
+                      >
+                        Khách sạn Hồ Chí Minh
+                      </span>
+                    </a>
+                  </span>
+                  <span class="css-901oao css-16my406 r-cwxd7f r-6koalj r-eqz5dr r-t1w4ow r-ubezar r-majxgm r-135wba7 r-fdjqy7 r-30z84g">
+                    <a href="https://travelago.vercel.app/" class="css-4rbku5" style={{ paddingBottom: "24px" }}>
+                      <span
+                        class="css-901oao css-16my406 css-cens5h r-t1w4ow r-1b43r93 r-majxgm r-rjixqe r-fdjqy7"
+                        style={{ WebkitLineClamp: 2, color: "rgb(104, 113, 118)" }}
+                      >
+                        Khách sạn Nha Trang
+                      </span>
+                    </a>
+                    <a href="https://travelago.vercel.app/" class="css-4rbku5" style={{ paddingBottom: "24px" }}>
+                      <span
+                        class="css-901oao css-16my406 css-cens5h r-t1w4ow r-1b43r93 r-majxgm r-rjixqe r-fdjqy7"
+                        style={{ WebkitLineClamp: 2, color: "rgb(104, 113, 118)" }}
+                      >
+                        Khách sạn Hội An
+                      </span>
+                    </a>
+                    <a href="https://travelago.vercel.app/" class="css-4rbku5" style={{ paddingBottom: "24px" }}>
+                      <span
+                        class="css-901oao css-16my406 css-cens5h r-t1w4ow r-1b43r93 r-majxgm r-rjixqe r-fdjqy7"
+                        style={{ WebkitLineClamp: 2, color: "rgb(104, 113, 118)" }}
+                      >
+                        Khách sạn Sa Pa
+                      </span>
+                    </a>
+                    <a href="https://travelago.vercel.app/" class="css-4rbku5" style={{ paddingBottom: "24px" }}>
+                      <span
+                        class="css-901oao css-16my406 css-cens5h r-t1w4ow r-1b43r93 r-majxgm r-rjixqe r-fdjqy7"
+                        style={{ WebkitLineClamp: 2, color: "rgb(104, 113, 118)" }}
+                      >
+                        Khách sạn Hà Nội
+                      </span>
+                    </a>
+                  </span>
+                  <span class="css-901oao css-16my406 r-cwxd7f r-6koalj r-eqz5dr r-t1w4ow r-ubezar r-majxgm r-135wba7 r-fdjqy7 r-30z84g">
+                    <a href="https://travelago.vercel.app/" class="css-4rbku5" style={{ paddingBottom: "24px" }}>
+                      <span
+                        class="css-901oao css-16my406 css-cens5h r-t1w4ow r-1b43r93 r-majxgm r-rjixqe r-fdjqy7"
+                        style={{ WebkitLineClamp: 2, color: "rgb(104, 113, 118)" }}
+                      >
+                        Khách sạn Phan Thiết
+                      </span>
+                    </a>
+                    <a href="https://travelago.vercel.app/" class="css-4rbku5" style={{ paddingBottom: "24px" }}>
+                      <span
+                        class="css-901oao css-16my406 css-cens5h r-t1w4ow r-1b43r93 r-majxgm r-rjixqe r-fdjqy7"
+                        style={{ WebkitLineClamp: 2, color: "rgb(104, 113, 118)" }}
+                      >
+                        Khách sạn Phú Quốc
+                      </span>
+                    </a>
+                    <a href="https://travelago.vercel.app/" class="css-4rbku5" style={{ paddingBottom: "24px" }}>
+                      <span
+                        class="css-901oao css-16my406 css-cens5h r-t1w4ow r-1b43r93 r-majxgm r-rjixqe r-fdjqy7"
+                        style={{ WebkitLineClamp: 2, color: "rgb(104, 113, 118)" }}
+                      >
+                        Khách sạn Quy Nhơn
+                      </span>
+                    </a>
+                    <a href="https://travelago.vercel.app/" class="css-4rbku5" style={{ paddingBottom: "24px" }}>
+                      <span
+                        className="css-901oao css-16my406 css-cens5h r-t1w4ow r-1b43r93 r-majxgm r-rjixqe r-fdjqy7"
+                        style={{ WebkitLineClamp: 2, color: "rgb(104, 113, 118)" }}
+                      >
+                        Vé máy bay Tết
+                      </span>
+                    </a>
+                  </span>
+                </div>
+              </div>
+              <div class="css-1dbjc4n r-13awgt0" style={{ opacity: 1, transform: "translateX(0px)", display: "none" }}>
+                <div
+                  dir="auto"
+                  class="css-901oao r-cwxd7f r-6koalj r-13awgt0 r-18u37iz r-t1w4ow r-ubezar r-majxgm r-1wtj0ep r-135wba7 r-11c0sde r-fdjqy7 r-13qz1uu"
+                ></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img
+          importance="low"
+          loading="lazy"
+          src="https://ik.imagekit.io/tvlk/image/imageResource/2023/04/21/1682061066590-85ee56e96e55a4f5afe6a04afe8c55da.png?tr=q-75"
+          srcset="https://ik.imagekit.io/tvlk/image/imageResource/2023/04/21/1682061066590-85ee56e96e55a4f5afe6a04afe8c55da.png?tr=q-75 1x, https://ik.imagekit.io/tvlk/image/imageResource/2023/04/21/1682061066590-85ee56e96e55a4f5afe6a04afe8c55da.png?tr=dpr-2,q-75 2x, https://ik.imagekit.io/tvlk/image/imageResource/2023/04/21/1682061066590-85ee56e96e55a4f5afe6a04afe8c55da.png?tr=dpr-3,q-75 3x"
+          decoding="async"
+          width="100%"
+          height="100%"
+          class="r-u8s1d r-1wyyakw"
+          style={{ objectFit: "contain", objectPosition: "50% 100%" }}
+        />
+      </div>
+    </div>
+  );
+}
+    
+export default Discover;
---
+++ src/components/uiHomepage/Reason.jsx
@@ -0,0 +1,125 @@
+import "./uiHomepage.scss";
+function Reason() {
+    return (
+        <div data-subtree="HomepageUSPV2">
+            <img
+                importance="low"
+                loading="lazy"
+                src="https://ik.imagekit.io/tvlk/image/imageResource/2023/11/17/1700215562769-2f362de4d3cf5253829ed5a07c10c17b.png?tr=q-75"
+                srcset="https://ik.imagekit.io/tvlk/image/imageResource/2023/11/17/1700215562769-2f362de4d3cf5253829ed5a07c10c17b.png?tr=q-75 1x, https://ik.imagekit.io/tvlk/image/imageResource/2023/11/17/1700215562769-2f362de4d3cf5253829ed5a07c10c17b.png?tr=dpr-2,q-75 2x, https://ik.imagekit.io/tvlk/image/imageResource/2023/11/17/1700215562769-2f362de4d3cf5253829ed5a07c10c17b.png?tr=dpr-3,q-75 3x"
+                decoding="async"
+                class="r-u8s1d"
+            />
+            <div class="css-1dbjc4n r-1kihuf0 r-cqs64b r-1fnihl3">
+                <div class="css-1dbjc4n r-eqz5dr">
+                    <h2
+                        aria-level="2"
+                        dir="auto"
+                        role="heading"
+                        class="css-4rbku5 css-901oao r-cwxd7f r-t1w4ow r-1x35g6 r-b88u0q r-ueyrd6 r-fdjqy7"
+                    >
+                        Lý do nên đặt chỗ với Travelago?
+                    </h2>
+                    <div class="css-1dbjc4n" style={{ paddingBottom: "24px" }}></div>
+                    <div class="css-1dbjc4n r-1awozwy r-18u37iz r-1h0z5md">
+
+                    <div class="css-1dbjc4n r-14lw9ot r-kdyh1x r-da5iq2 r-khh7iy r-1udh08x r-ymttw5 r-1f1sjgu">
+                        <div class="css-1dbjc4n r-1awozwy r-18u37iz r-1h0z5md">
+                            <div class="css-1dbjc4n">
+                                <img
+                                    importance="low"
+                                    loading="lazy"
+                                    src="https://ik.imagekit.io/tvlk/image/imageResource/2023/06/14/1686718236517-bf9a3e5ffd872b781ba2e56e93fe8840.webp?tr=h-64,q-75,w-64"
+                                    srcset="https://ik.imagekit.io/tvlk/image/imageResource/2023/06/14/1686718236517-bf9a3e5ffd872b781ba2e56e93fe8840.webp?tr=h-64,q-75,w-64 1x, https://ik.imagekit.io/tvlk/image/imageResource/2023/06/14/1686718236517-bf9a3e5ffd872b781ba2e56e93fe8840.webp?tr=dpr-2,h-64,q-75,w-64 2x, https://ik.imagekit.io/tvlk/image/imageResource/2023/06/14/1686718236517-bf9a3e5ffd872b781ba2e56e93fe8840.webp?tr=dpr-3,h-64,q-75,w-64 3x"
+                                    decoding="async"
+                                    width="64"
+                                    height="64"
+                                />
+                            </div>
+                            <div class="css-1dbjc4n" style={{ paddingRight: "0px" }}></div>
+                            <div class="css-1dbjc4n r-1w6e6rj r-1fz3rvf r-o6e10k">
+                                <h2
+                                    aria-level="2"
+                                    dir="auto"
+                                    role="heading"
+                                    class="css-4rbku5 css-901oao r-cwxd7f r-t1w4ow r-ubezar r-b88u0q r-135wba7 r-5oul0u r-fdjqy7"
+                                    data-testid="desktop-usp-item-text-0"
+                                >
+                                    <strong>Đáp ứng mọi nhu cầu của bạn</strong>
+                                </h2>
+                                <div dir="auto" class="css-901oao r-cwxd7f r-t1w4ow r-1b43r93 r-majxgm r-rjixqe r-fdjqy7">
+                                    Từ chuyến bay, lưu trú, đến điểm tham quan, bạn có thể tin chọn <strong>sản phẩm hoàn chỉnh</strong>{" "}
+                                    và <strong>Hướng Dẫn Du Lịch </strong> của chúng tôi.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="css-1dbjc4n" style={{ paddingRight: "16px" }}></div>
+
+                    <div class="css-1dbjc4n r-14lw9ot r-kdyh1x r-da5iq2 r-khh7iy r-1udh08x r-ymttw5 r-1f1sjgu">
+                        <div class="css-1dbjc4n">
+                            <img
+                                importance="low"
+                                loading="lazy"
+                                src="https://ik.imagekit.io/tvlk/image/imageResource/2023/06/14/1686718238370-9ef6b6e0701e9074614951c0bd49930c.webp?tr=h-64,q-75,w-64"
+                                srcset="https://ik.imagekit.io/tvlk/image/imageResource/2023/06/14/1686718238370-9ef6b6e0701e9074614951c0bd49930c.webp?tr=h-64,q-75,w-64 1x, https://ik.imagekit.io/tvlk/image/imageResource/2023/06/14/1686718238370-9ef6b6e0701e9074614951c0bd49930c.webp?tr=dpr-2,h-64,q-75,w-64 2x, https://ik.imagekit.io/tvlk/image/imageResource/2023/06/14/1686718238370-9ef6b6e0701e9074614951c0bd49930c.webp?tr=dpr-3,h-64,q-75,w-64 3x"
+                                decoding="async"
+                                width="64"
+                                height="64"
+                            />
+                        </div>
+                        <div class="css-1dbjc4n" style={{ paddingRight: "0px" }}></div>
+                        <div class="css-1dbjc4n r-1w6e6rj r-1fz3rvf r-o6e10k">
+                            <h2
+                                aria-level="2"
+                                dir="auto"
+                                role="heading"
+                                class="css-4rbku5 css-901oao r-cwxd7f r-t1w4ow r-ubezar r-b88u0q r-135wba7 r-5oul0u r-fdjqy7"
+                                data-testid="desktop-usp-item-text-1"
+                            >
+                                <strong>Tùy chọn đặt chỗ linh hoạt</strong>
+                            </h2>
+                            <div dir="auto" class="css-901oao r-cwxd7f r-t1w4ow r-1b43r93 r-majxgm r-rjixqe r-fdjqy7">
+                                Kế hoạch thay đổi bất ngờ? Đừng lo!<strong>Đổi lịch</strong> hoặc <strong>Hoàn tiền</strong> dễ dàng.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="css-1dbjc4n" style={{ paddingRight: "16px" }}></div>
+
+                    <div class="css-1dbjc4n r-14lw9ot r-kdyh1x r-da5iq2 r-khh7iy r-1udh08x r-ymttw5 r-1f1sjgu">
+                        <div class="css-1dbjc4n">
+                            <img
+                                importance="low"
+                                loading="lazy"
+                                src="https://ik.imagekit.io/tvlk/image/imageResource/2023/06/14/1686718240417-96131eb957c39a098a12d2d7926e3342.webp?tr=h-64,q-75,w-64"
+                                srcset="https://ik.imagekit.io/tvlk/image/imageResource/2023/06/14/1686718240417-96131eb957c39a098a12d2d7926e3342.webp?tr=h-64,q-75,w-64 1x, https://ik.imagekit.io/tvlk/image/imageResource/2023/06/14/1686718240417-96131eb957c39a098a12d2d7926e3342.webp?tr=dpr-2,h-64,q-75,w-64 2x, https://ik.imagekit.io/tvlk/image/imageResource/2023/06/14/1686718240417-96131eb957c39a098a12d2d7926e3342.webp?tr=dpr-3,h-64,q-75,w-64 3x"
+                                decoding="async"
+                                width="64"
+                                height="64"
+                            />
+                        </div>
+                        <div class="css-1dbjc4n" style={{ paddingRight: "0px" }}></div>
+                        <div class="css-1dbjc4n r-1w6e6rj r-1fz3rvf r-o6e10k">
+                            <h2
+                                aria-level="2"
+                                dir="auto"
+                                role="heading"
+                                class="css-4rbku5 css-901oao r-cwxd7f r-t1w4ow r-ubezar r-b88u0q r-135wba7 r-5oul0u r-fdjqy7"
+                                data-testid="desktop-usp-item-text-2"
+                            >
+                                <strong>Thanh toán an toàn và thuận tiện</strong>
+                            </h2>
+                            <div dir="auto" class="css-901oao r-cwxd7f r-t1w4ow r-1b43r93 r-majxgm r-rjixqe r-fdjqy7">
+                                Tận hưởng <strong>s nhiều cách thanh toán an toàn</strong>, bằng loại tiền thuận tiện nhất cho bạn.
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Reason;
